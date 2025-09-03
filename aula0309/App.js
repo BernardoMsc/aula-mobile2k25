03c9f7b3d803ff93162f8e79e04f57472c7d5f6c
BERNARDO MORSCHHEISER <bernardo.m@ACAD.UNIVATES>
2025-09-03 20:37:04 -0300

    corrigindo botões

--- aula0309/App.js
+++ aula0309/App.js
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <SafeAreaView style={estilos.areaSegura}>
       <Button title="Adicionar 1" onPress={() => setContador(contador + 1)}></Button>
-      <Button style={estilos.botao} title="Resetar" onPress={() => setContador(contador == 0)}></Button>
+      <Button title="Resetar" onPress={() => setContador(contador == 0)}></Button>
       <Button title="Diminuir 1" onPress={() => setContador(contador - 1)}></Button>
       <Text style={estilos.texto}>Contador: {contador}</Text>
     </SafeAreaView>
