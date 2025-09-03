02ae4382897d62c26b9e65ca9f898f5bb7afa9d7
BERNARDO MORSCHHEISER <bernardo.m@ACAD.UNIVATES>
2025-09-03 20:34:40 -0300

    adicionando botões

---
+++ aula0309/App.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function App() {
+
+  const [contador, setContador] = useState(0);
+
+  return (
+    <SafeAreaView style={estilos.areaSegura}>
+      <Button title="Adicionar 1" onPress={() => setContador(contador + 1)}></Button>
+      <Button style={estilos.botao} title="Resetar" onPress={() => setContador(contador == 0)}></Button>
+      <Button title="Diminuir 1" onPress={() => setContador(contador - 1)}></Button>
+      <Text style={estilos.texto}>Contador: {contador}</Text>
+    </SafeAreaView>
+  );
+}
+
+const estilos = StyleSheet.create({
+  texto: {
+    fontSize: 20,
+  }
+});
+
+
