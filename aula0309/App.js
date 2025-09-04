1397104ce2cd0bc34723ed8a5631712fa4e1b0cc
BERNARDO MORSCHHEISER <bernardo.m@ACAD.UNIVATES>
2025-09-03 22:05:36 -0300

    atividade da calculadora

--- aula0309/App.js
+++ aula0309/App.js
@@ -1,24 +1,91 @@
 import { useState } from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, TextInput } from "react-native";
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
 
-  const [contador, setContador] = useState(0);
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+  const [result, setResult] = useState(0);
+
+  function somar(num1, num2) {
+    const n1Convertido = parseFloat(num1);
+    const n2Convertido = parseFloat(num2);
+    const soma = n1Convertido + n2Convertido;
+    return soma;
+  }
+
+  function subtrair(num1, num2) {
+    const n1Convertido = parseFloat(num1);
+    const n2Convertido = parseFloat(num2);
+    const subtrair = n1Convertido - n2Convertido;
+    return subtrair;
+  }
+
+  function multiplicar(num1, num2) {
+    const n1Convertido = parseFloat(num1);
+    const n2Convertido = parseFloat(num2);
+    const multiplicar = n1Convertido * n2Convertido;
+    return multiplicar;
+  }
+
+    function dividir(num1, num2) {
+    const n1Convertido = parseFloat(num1);
+    const n2Convertido = parseFloat(num2);
+    let div = n1Convertido / n2Convertido;
+    if (n2Convertido == 0) {
+      div = "Não pode dividir por 0!!";
+    }
+    return div;
+  }
 
   return (
     <SafeAreaView style={estilos.areaSegura}>
-      <Button title="Adicionar 1" onPress={() => setContador(contador + 1)}></Button>
-      <Button title="Resetar" onPress={() => setContador(contador == 0)}></Button>
-      <Button title="Diminuir 1" onPress={() => setContador(contador - 1)}></Button>
-      <Text style={estilos.texto}>Contador: {contador}</Text>
+      <TextInput
+        style={estilos.campo}
+        placeholder="Digite um número:"
+        keyboardType="numeric"
+        onChangeText={setNum1}
+        value={num1}
+      />
+      <TextInput
+        style={estilos.campo}
+        placeholder="Digite um número:"
+        keyboardType="numeric"
+        onChangeText={setNum2}
+        value={num2}
+      />
+
+      <View style={estilos.botao}>
+        <Button title=" + " onPress={() => setResult(somar(num1, num2))} />
+        <Button title=" - " onPress={() => setResult(subtrair(num1, num2))} />
+        <Button title=" * " onPress={() => setResult(multiplicar(num1, num2))} />
+        <Button title=" / " onPress={() => setResult(dividir(num1, num2))} />
+      </View>
+      <Button title="Resetar" onPress={() => setResult(result == 0)}></Button>
+      <Text style={estilos.texto}>Resultado: {result}</Text>
     </SafeAreaView>
   );
 }
 
 const estilos = StyleSheet.create({
+  areaSegura: {
+    padding: 10,
+    flex: 1,
+  },
   texto: {
     fontSize: 20,
+  },
+  campo: {
+    borderWidth: 1,
+    borderRadius: 10
+  },
+  botao: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    padding: 10,
+    marginTop: "10",
   }
 });
 
